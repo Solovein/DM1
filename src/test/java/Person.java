bac528de463fa67ea3f4cf7bf46c86c3f0b30ca8
solovei <soloveykon@gmail.com>
2023-06-03 00:26:55 +0300

    Add fields for class Person

--- src/test/java/Person.java
+++ src/test/java/Person.java
@@ -1,2 +1,27 @@
-package test.java;public class Person {
+package test.java;
+
+public class Person {
+    private String name;
+    private String lastName;
+
+    public Person(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
